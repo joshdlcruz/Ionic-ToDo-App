1a943f98c489e8602d2bcf94f2bdc2b86c309f2e
joshdlcruz <40876758+joshdlcruz@users.noreply.github.com>
2019-07-17 14:42:45 +0800

    add task function

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  todosRef: AngularFireList<any>;
+  todos: Observable<any[]>;
+  // input value
+  newTodo = { task: '' };
 
-  constructor() {}
+  constructor(public db: AngularFireDatabase) {
+    this.todosRef = db.list('/todo');
 
+    this.todos = this.todosRef
+      .snapshotChanges()
+      .pipe(
+        map(changes =>
+          changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+        )
+      );
+  }
+  addTask(newTodo) {
+    this.todosRef.push(newTodo);
+    // empty the input
+    this.newTodo = { task: '' };
+  }
 }
